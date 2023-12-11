465ec2a474f00bd7d80ebc43a5fb7cb7ab57cfd0
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-12-12 00:02:29 +0300

    refined convert on am and pm

--- convert_hours.py
+++ convert_hours.py
@@ -1,20 +1,25 @@
 def convert_to_24_hrs(hh = 0, mm = 0, time = ''):
     if (hh >= 1 and hh <= 12) and (mm >= 00 and mm <=59) and (time == 'am' or time == 'pm'):
-        time = 'hrs'
         if time == 'am':
-            print(f'{hh}{mm}{time}')
+            if hh <= 9:
+                time = 'hrs'
+                print(f'0{hh}{mm}{time} 1')
+            else:
+                time = 'hrs'
+                print(f'{hh}{mm}{time} 2')
         else:
             hh += 12
             mm += 00
-            print(f'{hh}{mm}{time}')
+            time = 'hrs'
+            print(f'{hh}{mm}{time} 3')
         pass
     else:
-        print('failed')
+        print('enter valid time')
     pass
 
-convert_to_24_hrs(11, 36, 'am')
-convert_to_24_hrs(11, 36, 'pm')
-convert_to_24_hrs(11, 36, 'to')
-convert_to_24_hrs(13, 36, 'am')
-convert_to_24_hrs(11, 70, 'am')
-convert_to_24_hrs(12, 60, 'am')
+# convert_to_24_hrs(1, 36, 'am')
+# convert_to_24_hrs(6, 36, 'am')
+# convert_to_24_hrs(12, 36, 'am')
+# convert_to_24_hrs(1, 36, 'pm')
+# convert_to_24_hrs(6, 36, 'pm')
+# convert_to_24_hrs(12, 36, 'pm')
